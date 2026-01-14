0a914955b69a3d513d1e493a2f4265ce27f96ac0
PnjDark <patrickjnarmaye@gmail.com>
2026-01-15 00:13:14 +0100

    Setup academic and admin_hr apps, and create a virtual environment

---
+++ admin_hr/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdminHrConfig(AppConfig):
+    name = 'admin_hr'
---
+++ finance_marketing/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FinanceMarketingConfig(AppConfig):
+    name = 'finance_marketing'
